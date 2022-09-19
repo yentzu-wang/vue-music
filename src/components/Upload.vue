c2e275b3cf90cd8bc42ecfa9592339ccf532a38b
Frank Wang <frank7300228@gmail.com>
2022-09-20 01:12:11 +0800

    Finish 155

---
+++ src/components/Upload.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="relative flex flex-col rounded border border-gray-200 bg-white">
+    <div class="border-b border-gray-200 px-6 pt-6 pb-5 font-bold">
+      <span class="card-title">Upload</span>
+      <i class="fas fa-upload float-right text-2xl text-green-400"></i>
+    </div>
+    <div class="p-6">
+      <!-- Upload Dropbox -->
+      <div
+        class="w-full cursor-pointer rounded border border-dashed border-gray-400 px-10 py-20 text-center text-gray-400 transition duration-500 hover:border-solid hover:border-green-400 hover:bg-green-400 hover:text-white"
+      >
+        <h5>Drop your files here</h5>
+      </div>
+      <hr class="my-6" />
+      <!-- Progess Bars -->
+      <div class="mb-4">
+        <!-- File Name -->
+        <div class="text-sm font-bold">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden rounded bg-gray-200">
+          <!-- Inner Progress Bar -->
+          <div
+            class="progress-bar bg-blue-400 transition-all"
+            style="width: 75%"
+          ></div>
+        </div>
+      </div>
+      <div class="mb-4">
+        <div class="text-sm font-bold">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden rounded bg-gray-200">
+          <div
+            class="progress-bar bg-blue-400 transition-all"
+            style="width: 35%"
+          ></div>
+        </div>
+      </div>
+      <div class="mb-4">
+        <div class="text-sm font-bold">Just another song.mp3</div>
+        <div class="flex h-4 overflow-hidden rounded bg-gray-200">
+          <div
+            class="progress-bar bg-blue-400 transition-all"
+            style="width: 55%"
+          ></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -3,53 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <div
-          class="relative flex flex-col rounded border border-gray-200 bg-white"
-        >
-          <div class="border-b border-gray-200 px-6 pt-6 pb-5 font-bold">
-            <span class="card-title">Upload</span>
-            <i class="fas fa-upload float-right text-2xl text-green-400"></i>
-          </div>
-          <div class="p-6">
-            <!-- Upload Dropbox -->
-            <div
-              class="w-full cursor-pointer rounded border border-dashed border-gray-400 px-10 py-20 text-center text-gray-400 transition duration-500 hover:border-solid hover:border-green-400 hover:bg-green-400 hover:text-white"
-            >
-              <h5>Drop your files here</h5>
-            </div>
-            <hr class="my-6" />
-            <!-- Progess Bars -->
-            <div class="mb-4">
-              <!-- File Name -->
-              <div class="text-sm font-bold">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden rounded bg-gray-200">
-                <!-- Inner Progress Bar -->
-                <div
-                  class="progress-bar bg-blue-400 transition-all"
-                  style="width: 75%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="text-sm font-bold">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden rounded bg-gray-200">
-                <div
-                  class="progress-bar bg-blue-400 transition-all"
-                  style="width: 35%"
-                ></div>
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="text-sm font-bold">Just another song.mp3</div>
-              <div class="flex h-4 overflow-hidden rounded bg-gray-200">
-                <div
-                  class="progress-bar bg-blue-400 transition-all"
-                  style="width: 55%"
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Upload />
       </div>
       <div class="col-span-2">
         <div
@@ -178,3 +132,7 @@
     </div>
   </section>
 </template>
+
+<script setup>
+import Upload from "@/components/Upload.vue"
+</script>
