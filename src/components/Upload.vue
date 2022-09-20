36a2636c455761ab7d765678ec54f41c68dff11b
Frank Wang <frank7300228@gmail.com>
2022-09-21 02:43:09 +0800

    Finish 167

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -15,10 +15,11 @@
         @dragover.prevent.stop="isDragOver = true"
         @dragenter.prevent.stop="isDragOver = true"
         @dragleave.prevent.stop="isDragOver = false"
-        @drop.prevent.stop="upload"
+        @drop.prevent.stop="dragUpload"
       >
         <h5>Drop your files here</h5>
       </div>
+      <input type="file" multiple @change="upload" />
       <hr class="my-6" />
       <!-- Progess Bars -->
       <div
@@ -44,76 +45,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive } from "vue"
-import { storage, UploadTask, auth, songsCollection } from "@/includes/firebase"
+import { useAudioUpload } from "@/composables/useAudioUpload"
 
-type Upload = {
-  task: UploadTask
-  name: string
-  progress: number
-  variant: string
-  icon: string
-  textClass: string
-}
-
-const isDragOver = ref(false)
-const uploads = reactive<Upload[]>([])
-
-const upload = (e: DragEvent) => {
-  isDragOver.value = false
-  const files = e.dataTransfer ? [...e.dataTransfer.files] : []
-
-  files.forEach((file) => {
-    if (file.type !== "audio/mpeg") {
-      console.log("Only mp3 files are allowed")
-      return
-    }
-
-    const storageRef = storage.ref()
-    const songsRef = storageRef.child(`songs/${file.name}`)
-    const task = songsRef.put(file)
-
-    const uploadIndex =
-      uploads.push({
-        task,
-        name: file.name,
-        progress: 0,
-        variant: "bg-blue-400",
-        icon: "fas fa-spinner fa-spin",
-        textClass: ""
-      }) - 1
-
-    task.on(
-      "state_changed",
-      (snapshot) => {
-        uploads[uploadIndex].progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      },
-      (error) => {
-        uploads[uploadIndex].variant = "bg-red-400"
-        uploads[uploadIndex].icon = "fas fa-times"
-        uploads[uploadIndex].textClass = "text-red-400"
-        console.log(error)
-      },
-      async () => {
-        const song = {
-          uid: auth.currentUser?.uid,
-          displayName: auth.currentUser?.displayName,
-          originalName: task.snapshot.ref.name,
-          modifiedName: task.snapshot.ref.name,
-          genre: "",
-          commentCount: 0,
-          url: ""
-        }
-
-        song.url = await task.snapshot.ref.getDownloadURL()
-        await songsCollection.add(song)
-
-        uploads[uploadIndex].variant = "bg-green-400"
-        uploads[uploadIndex].icon = "fas fa-check"
-        uploads[uploadIndex].textClass = "text-green-400"
-      }
-    )
-  })
-}
+const { isDragOver, uploads, upload, dragUpload } = useAudioUpload()
 </script>
---
+++ src/composables/useAudioUpload.ts
@@ -0,0 +1,87 @@
+import { ref, reactive } from "vue"
+import { storage, UploadTask, auth, songsCollection } from "@/includes/firebase"
+
+export const useAudioUpload = () => {
+  type Upload = {
+    task: UploadTask
+    name: string
+    progress: number
+    variant: string
+    icon: string
+    textClass: string
+  }
+
+  const isDragOver = ref(false)
+  const uploads = reactive<Upload[]>([])
+
+  const upload = (e: Event) => {
+    const files = (e.target as HTMLInputElement).files || []
+
+    processUpload([...files])
+  }
+
+  const dragUpload = (e: DragEvent) => {
+    isDragOver.value = false
+
+    const files = e.dataTransfer ? [...e.dataTransfer.files] : []
+
+    processUpload(files)
+  }
+
+  function processUpload(files: File[]) {
+    files.forEach((file) => {
+      if (file.type !== "audio/mpeg") {
+        console.log("Only mp3 files are allowed")
+        return
+      }
+
+      const storageRef = storage.ref()
+      const songsRef = storageRef.child(`songs/${file.name}`)
+      const task = songsRef.put(file)
+
+      const uploadIndex =
+        uploads.push({
+          task,
+          name: file.name,
+          progress: 0,
+          variant: "bg-blue-400",
+          icon: "fas fa-spinner fa-spin",
+          textClass: ""
+        }) - 1
+
+      task.on(
+        "state_changed",
+        (snapshot) => {
+          uploads[uploadIndex].progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        },
+        (error) => {
+          uploads[uploadIndex].variant = "bg-red-400"
+          uploads[uploadIndex].icon = "fas fa-times"
+          uploads[uploadIndex].textClass = "text-red-400"
+          console.log(error)
+        },
+        async () => {
+          const song = {
+            uid: auth.currentUser?.uid,
+            displayName: auth.currentUser?.displayName,
+            originalName: task.snapshot.ref.name,
+            modifiedName: task.snapshot.ref.name,
+            genre: "",
+            commentCount: 0,
+            url: ""
+          }
+
+          song.url = await task.snapshot.ref.getDownloadURL()
+          await songsCollection.add(song)
+
+          uploads[uploadIndex].variant = "bg-green-400"
+          uploads[uploadIndex].icon = "fas fa-check"
+          uploads[uploadIndex].textClass = "text-green-400"
+        }
+      )
+    })
+  }
+
+  return { isDragOver, uploads, upload, dragUpload }
+}
