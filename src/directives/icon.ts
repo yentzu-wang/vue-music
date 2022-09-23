c2ee79df2c15aff8a1c8baf82978d5fb7fee257d
Frank Wang <frank7300228@gmail.com>
2022-09-24 07:21:27 +0800

    Finish 206

--- src/directives/icon.ts
+++ src/directives/icon.ts
@@ -1,6 +1,11 @@
 export default {
-  beforeMount(el) {
-    el.innerHTML +=
-      '<i class="fa fa-headphones-alt float-right text-xl text-green-400"></i>'
+  beforeMount(el, binding) {
+    let iconClass = `fa fa-${binding.value} float-right text-xl text-green-400`
+
+    if (binding.arg === "full") {
+      iconClass = binding.value
+    }
+
+    el.innerHTML += `<i class="${iconClass}"></i>`
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,7 +29,10 @@
       <div
         class="relative flex flex-col rounded border border-gray-200 bg-white"
       >
-        <div class="border-b border-gray-200 px-6 pt-6 pb-5 font-bold" v-icon>
+        <div
+          class="border-b border-gray-200 px-6 pt-6 pb-5 font-bold"
+          v-icon="'headphones-alt'"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
         </div>
