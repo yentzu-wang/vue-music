7caa1e3c0963667ab771d01a1ac2d977d01788ac
Frank Wang <frank7300228@gmail.com>
2022-09-24 02:48:05 +0800

    Finish 195

--- src/stores/player.ts
+++ src/stores/player.ts
@@ -1,12 +1,25 @@
 import { ref } from "vue"
 import { defineStore } from "pinia"
+import { Howl } from "howler"
 import { ISong } from "@/composables/useMusicDetail"
 
 export const usePlayerStore = defineStore("player", () => {
   const currentSong = ref<ISong>()
+  const sound = ref<Howl>()
 
   async function newSong(song: ISong | undefined) {
+    if (!song) {
+      return
+    }
+
     currentSong.value = song
+
+    sound.value = new Howl({
+      src: [song?.url || ""],
+      html5: true
+    })
+
+    sound.value.play()
   }
 
   return { currentSong, newSong }
