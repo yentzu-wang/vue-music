107fdd71e8e0a4fa9e343e5bb0631c648c2be979
Frank Wang <frank7300228@gmail.com>
2022-09-25 23:00:05 +0800

    Finish 241

---
+++ src/components/__tests__/songitem.spec.ts
@@ -0,0 +1,29 @@
+import { describe, test, expect } from "vitest"
+import { shallowMount, RouterLinkStub } from "@vue/test-utils"
+import SongItem from "@/components/SongItem.vue"
+
+describe("SongItem.vue", () => {
+  const song = {
+    docId: "docId",
+    modifiedName: "modifiedName",
+    displayName: "test",
+    commentCount: 0
+  }
+
+  test("render song.displayName", () => {
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song
+      },
+      global: {
+        components: {
+          "router-link": RouterLinkStub
+        }
+      }
+    })
+
+    const compositionAuthor = wrapper.find(".text-gray-500")
+
+    expect(compositionAuthor.text()).toBe(`Upload by ${song.displayName}`)
+  })
+})
