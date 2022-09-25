a70afa153d9274a0c5dbe417505b839e2c56749d
Frank Wang <frank7300228@gmail.com>
2022-09-25 20:08:53 +0800

    Finish 228

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router"
-import Home from "@/views/Home.vue"
 import { useProtectedContent } from "@/composables/useProtectedContent"
+
+const Home = () => import("@/views/Home.vue")
+const About = () => import("@/views/About.vue")
+const Manage = () => import("@/views/Manage.vue")
+const Song = () => import("@/views/Song.vue")
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,13 +21,13 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("@/views/About.vue")
+      component: About
     },
     {
       path: "/manage-music",
       // alias: "/manage",
       name: "manage",
-      component: () => import("@/views/Manage.vue"),
+      component: Manage,
       meta: {
         requiresAuth: true
       }
@@ -32,7 +36,7 @@ const router = createRouter({
     {
       name: "song",
       path: "/song/:id",
-      component: () => import("@/views/Song.vue")
+      component: Song
     },
     { path: "/:catchAll(.*)*", redirect: { name: "home" } }
   ]
