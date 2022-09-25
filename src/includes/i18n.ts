4fde6d9d9936fc00ad496a39170ff01116baa387
Frank Wang <frank7300228@gmail.com>
2022-09-25 12:25:15 +0800

    Finish 210

--- src/includes/i18n.ts
+++ src/includes/i18n.ts
@@ -1,10 +1,14 @@
 import { createI18n } from "vue-i18n"
 import en from "@/locales/en.json"
+import zhTw from "@/locales/zh-tw.json"
+import zhCn from "@/locales/zh-cn.json"
 
 export default createI18n({
   locale: "en",
-  fallbackLOcale: "en",
+  fallbackLocale: "en",
   messages: {
-    en
+    en,
+    "zh-tw": zhTw,
+    "zh-cn": zhCn
   }
 })
